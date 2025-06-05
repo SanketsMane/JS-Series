ed96cc112094f3c7a419e4d5f0eb0e541759b476
Sanketmane <63346773+SanketsMane@users.noreply.github.com>
2025-06-05 05:13:34 +0000

    async-3

---
+++ 09_Async_Code/one.js
@@ -0,0 +1,17 @@
+//java script is asynchronous by default
+//synchronous code is executed in the order it is written
+
+//it is an single threaded language 
+//it can only do one thing at a time
+
+// @ execution context - 
+// @ Blocking Code - 
+//      1. block the flow of execution 2. Read file synchronously
+//      3. Write file synchronously
+//@ Non-blocking Code -
+//  1. does not block the flow of execution 2. Read file asynchronously
+//  3. Write file asynchronously
+
+
+
+
